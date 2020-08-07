b3bad67014ee49575817c4c123072b2c341609b2
jackrhumphrey <jack.humphrey@xtra.co.nz>
2020-08-07 12:51:26 +1200

    name

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <img src="https://laylahair.com/wp-content/uploads/2018/07/katy-perry-without-makeup-good-morning.jpg"></img>
-    </>
-  );
+  return <>Imy Blinky</>;
 }
 
 export default App;
