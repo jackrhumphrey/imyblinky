9032e822219a5f4ff10c46eb80d5507b5a3d0e23
jackrhumphrey <jack.humphrey@xtra.co.nz>
2020-08-08 14:59:28 +1200

    progress

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <>Imy Blinky</>;
+  return (
+    <div className="container">
+      <div className="splash">
+        <div className="imy">Imy Blinky</div>
+      </div>
+      <div className="content">
+        <div className="about">
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel
+          consequat dolor, a convallis velit. Morbi sagittis massa ac leo
+          consectetur, sed sagittis lorem lobortis. Integer nisl eros, rhoncus
+          varius dictum sit amet, blandit at risus. Duis laoreet magna eget
+          blandit pretium. Integer semper nibh quis sodales commodo. Ut enim
+          risus, aliquam ac lorem eget, elementum iaculis metus. Nulla facilisi.
+          Mauris sed consequat tortor. Mauris a quam non magna vulputate
+          sollicitudin.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
