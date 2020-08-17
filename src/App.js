45a1217cbfeee86fa26922a73fe4edee9439b442
jackrhumphrey <jack.humphrey@xtra.co.nz>
2020-08-17 23:04:18 +1200

    bleh

--- src/App.js
+++ src/App.js
@@ -7,18 +7,61 @@ function App() {
       <div className="splash">
         <div className="imy">Imy Blinky</div>
       </div>
-      <div className="content">
-        <div className="about">
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel
-          consequat dolor, a convallis velit. Morbi sagittis massa ac leo
-          consectetur, sed sagittis lorem lobortis. Integer nisl eros, rhoncus
-          varius dictum sit amet, blandit at risus. Duis laoreet magna eget
-          blandit pretium. Integer semper nibh quis sodales commodo. Ut enim
-          risus, aliquam ac lorem eget, elementum iaculis metus. Nulla facilisi.
-          Mauris sed consequat tortor. Mauris a quam non magna vulputate
-          sollicitudin.
-          </p>
+      <div className="main">
+        <div className="content">
+          <div className="about">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+              sed libero in odio posuere vulputate. Praesent interdum ipsum mi,
+              ac consectetur urna rutrum a. Donec sit amet tincidunt nisi.
+              Mauris consequat sit amet diam sit amet vulputate. Quisque orci
+              leo, ullamcorper convallis urna placerat, rutrum suscipit eros.
+              Integer et placerat massa, a vestibulum quam. Phasellus malesuada
+              maximus justo, pretium volutpat ligula pretium ut. Sed vel nisl
+              tincidunt, convallis arcu quis, malesuada ante. Aenean sit amet
+              feugiat mi. Aliquam in quam vel justo venenatis efficitur. Sed
+              mattis efficitur diam, et iaculis arcu condimentum et. Suspendisse
+              quis ultricies sem. Quisque vestibulum condimentum vulputate.
+            </p>
+          </div>
+          <div className="links">
+            <p>
+              <span className="link">
+                <a href="https://open.spotify.com/artist/2xUU5XZ4cHaQGl7NAlgMNJ?si=Oy8E_Up3RM-M2WJA87XHEw">
+                  Spotify
+                </a>
+              </span>
+              <br></br>
+              <span className="link">
+                <a href="https://imyblinky.bandcamp.com">Bandcamp</a>
+              </span>
+              <br></br>
+              <span className="link">
+                <a href="https://soundcloud.com/imy-blinky">SoundCloud</a>
+              </span>
+              <br></br>
+              <span className="link">
+                <a href="https://open.spotify.com/artist/2xUU5XZ4cHaQGl7NAlgMNJ?si=Oy8E_Up3RM-M2WJA87XHEw">
+                  triple j Unearthed
+                </a>
+              </span>
+            </p>
+          </div>
+          <div className="contact">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+              sed libero in odio posuere vulputate. Praesent interdum ipsum mi,
+              ac consectetur urna rutrum a. Donec sit amet tincidunt nisi.
+              Mauris consequat sit amet diam sit amet vulputate. Quisque orci
+              leo, ullamcorper convallis urna placerat, rutrum suscipit eros.
+              Integer et placerat massa, a vestibulum quam. Phasellus malesuada
+              maximus justo, pretium volutpat ligula pretium ut. Sed vel nisl
+              tincidunt, convallis arcu quis, malesuada ante. Aenean sit amet
+              feugiat mi. Aliquam in quam vel justo venenatis efficitur. Sed
+              mattis efficitur diam, et iaculis arcu condimentum et. Suspendisse
+              quis ultricies sem. Quisque vestibulum condimentum vulputate.
+            </p>
+          </div>
         </div>
       </div>
     </div>
