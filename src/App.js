1731b43e1a7a1c3f04a7a175ac0e7738c20ccf77
jackrhumphrey <jack.humphrey@xtra.co.nz>
2020-08-24 16:43:37 +1200

    styling

--- src/App.js
+++ src/App.js
@@ -37,37 +37,19 @@ function App() {
       </div>
       <div className="main">
         <div className="content">
-          <div className="about">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              sed libero in odio posuere vulputate. Praesent interdum ipsum mi,
-              ac consectetur urna rutrum a. Donec sit amet tincidunt nisi.
-              Mauris consequat sit amet diam sit amet vulputate. Quisque orci
-              leo, ullamcorper convallis urna placerat, rutrum suscipit eros.
-              Integer et placerat massa, a vestibulum quam. Phasellus malesuada
-              maximus justo, pretium volutpat ligula pretium ut. Sed vel nisl
-              tincidunt, convallis arcu quis, malesuada ante. Aenean sit amet
-              feugiat mi. Aliquam in quam vel justo venenatis efficitur. Sed
-              mattis efficitur diam, et iaculis arcu condimentum et. Suspendisse
-              quis ultricies sem. Quisque vestibulum condimentum vulputate.
-            </p>
-          </div>
           <div className="links">
             <p>
               <span className="link">
                 <a href="https://open.spotify.com/artist/2xUU5XZ4cHaQGl7NAlgMNJ?si=Oy8E_Up3RM-M2WJA87XHEw">
                   Spotify
                 </a>
-              </span>
-              <br></br>
+              </span>{" "}
               <span className="link">
                 <a href="https://imyblinky.bandcamp.com">Bandcamp</a>
-              </span>
-              <br></br>
+              </span>{" "}
               <span className="link">
                 <a href="https://soundcloud.com/imy-blinky">SoundCloud</a>
-              </span>
-              <br></br>
+              </span>{" "}
               <span className="link">
                 <a href="https://www.triplejunearthed.com/artist/imy-blinky">
                   triple j Unearthed
@@ -75,7 +57,30 @@ function App() {
               </span>
             </p>
           </div>
+
+          {/* <div className="about">
+            <p>
+            </p>
+          </div> */}
+          <div className="wrapper">
+          <iframe
+            src="https://open.spotify.com/embed/artist/2xUU5XZ4cHaQGl7NAlgMNJ"
+            width="300"
+            height="380"
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+          ></iframe>
           <div className="contact">
+            <div
+              style={{
+                marginBottom: "14px",
+                fontWeight: "bold",
+                color: "rgba(0, 0, 0, 0.87)",
+              }}
+            >
+              Contact
+            </div>
             <Form onSubmit={handleSubmit}>
               <input type="hidden" name="form-name" value="contact" />
               <Form.Input
@@ -100,6 +105,7 @@ function App() {
               />
               <Button type="submit">Submit</Button>
             </Form>
+          </div>
           </div>
         </div>
       </div>
